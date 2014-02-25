50224ffff52b730e217327bb3a11f9e31c5c25d9
Jeffrey Walraven <jeffrey@ravenswall.net>
2014-02-24 21:22:18 -0500

    Added basic app feed classes to hold the information from an app feed.

---
+++ src/main/java/org/jautoupdater/rss/AppFeedItem.java
@@ -0,0 +1,18 @@
+package org.jautoupdater.rss;
+
+/**
+* Holds information about an app RSS feed item. This
+* contains all the needed variables that the RSS
+* feed may contain.
+*
+* @author Jeffrey Walraven
+*/
+public class AppFeedItem {
+    
+    String title;
+    String description;
+    String publishDate;
+    String releaseNotesLink;
+    AppFeedUpdate update;
+
+}
---
+++ src/main/java/org/jautoupdater/rss/SignatureType.java
@@ -0,0 +1,11 @@
+package org.jautoupdater.rss;
+
+/**
+* An enum describing holding the values of different
+* signature types.
+*
+* @author Jeffrey Walraven
+*/
+public enum SignatureType {
+    PGP, DSA
+}
